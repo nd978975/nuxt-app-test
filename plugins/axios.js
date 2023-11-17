d839d16a674973833091266a73cabb8dbf79319d
Nam Dao <nd978975@gmail.com>
2023-11-17 13:32:59 +0700

    nuxt-app-test

--- plugins/axios.js
+++ plugins/axios.js
@@ -11,8 +11,8 @@ export default defineNuxtPlugin(nuxtApp => {
   //   axios.defaults.headers.common['Authorization'] = `Bearer ${token.value}`
   //   axios.defaults.headers['X-XSRF-TOKEN'] = token.value
   // }
-  // axios.defaults.headers.post['Content-Type'] = 'application/json';
-  // axios.defaults.headers.post['Accept'] = 'application/json';
+  axios.defaults.headers.post['Content-Type'] = 'application/json';
+  axios.defaults.headers.post['Accept'] = 'application/json';
 
 
   return {
