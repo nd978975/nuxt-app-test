16b3939c31469363dca532dcda81f535cb867eb6
Nam Dao <nd978975@gmail.com>
2023-11-17 11:59:16 +0700

    nuxt-app-test

--- plugins/axios.js
+++ plugins/axios.js
@@ -5,14 +5,14 @@ export default defineNuxtPlugin(nuxtApp => {
   axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
   axios.defaults.withCredentials = true
   axios.defaults.baseURL = "https://api.dodungchobe.xyz"
-  // axios.defaults.baseURL = "https://api.chamsocmevabe.top"
+  // axios.defaults.baseURL = "http://localhost:8000"
   // const token = useCookie("XSRF-TOKEN")
   // if (token.value) {
   //   axios.defaults.headers.common['Authorization'] = `Bearer ${token.value}`
   //   axios.defaults.headers['X-XSRF-TOKEN'] = token.value
   // }
-  axios.defaults.headers.post['Content-Type'] = 'application/json';
-  axios.defaults.headers.post['Accept'] = 'application/json';
+  // axios.defaults.headers.post['Content-Type'] = 'application/json';
+  // axios.defaults.headers.post['Accept'] = 'application/json';
 
 
   return {
